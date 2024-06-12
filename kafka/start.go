b32c5c27751e5f3fd932a77d1aee378e03c6229f
Germain-L <germain.leignel@gmail.com>
2024-06-12 12:14:34 +0200

    chore: Update Kubernetes deployment and main.go to include Kafka host
    This commit updates the Kubernetes deployment configuration in `deployment.yaml` to include a new environment variable `KAFKA_HOST` with the value "kafka-controller-headless.payetonkawa-prod.svc.cluster.local". Additionally, the `start.go` file is modified to retrieve the `KAFKA_USERNAME` and `KAFKA_PASSWORD` environment variables and log an error if they are not set. This change ensures that the application can connect to Kafka and consume messages in the production environment.

--- kafka/start.go
+++ kafka/start.go
@@ -2,16 +2,28 @@ package kafka
 
 import (
 	"log"
+	"os"
 
 	"github.com/confluentinc/confluent-kafka-go/v2/kafka"
 )
 
 func StartConsumer(brokers string, groupId string, topics []string, processMessage func(*kafka.Message)) {
+	username := os.Getenv("KAFKA_USERNAME")
+	password := os.Getenv("KAFKA_PASSWORD")
+
+	if username == "" || password == "" {
+		log.Fatal("Kafka username or password not set")
+	}
+
 	c, err := kafka.NewConsumer(&kafka.ConfigMap{
-		"bootstrap.servers":  brokers, // Use the passed brokers address
+		"bootstrap.servers":  brokers,
 		"group.id":           groupId,
 		"auto.offset.reset":  "earliest",
 		"enable.auto.commit": false,
+		"security.protocol":  "SASL_SSL", // or "SASL_PLAINTEXT" if SSL is not used
+		"sasl.mechanism":     "PLAIN",
+		"sasl.username":      username, // replace with your SASL username
+		"sasl.password":      password, // replace with your SASL password
 	})
 
 	if err != nil {
